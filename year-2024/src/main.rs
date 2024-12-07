19a4ea76e63062e51e6c885884db65fa86537a7a
Mintia11 <129310461+Mintia11@users.noreply.github.com>
2024-12-07 20:42:33 +0100

    All: Add day-7 solution

--- year-2024/src/main.rs
+++ year-2024/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
 }
 
 #[rustfmt::skip]
-const DAYS: &[fn()] = &[day_1::run, day_2::run, day_3::run, day_4::run, day_5::run, day_6::run];
+const DAYS: &[fn()] = &[day_1::run, day_2::run, day_3::run, day_4::run, day_5::run, day_6::run, day_7::run];
 
 #[path = "bin/day-1.rs"]
 mod day_1;
@@ -24,3 +24,6 @@ mod day_5;
 
 #[path = "bin/day-6.rs"]
 mod day_6;
+
+#[path = "bin/day-7.rs"]
+mod day_7;
